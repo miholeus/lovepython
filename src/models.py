87432fd378d6fbc73cf855bcdc3989b77931dad5
miholeus <me@miholeus.com>
2016-01-13 15:26:57 +0300

    votes

--- src/models.py
+++ src/models.py
@@ -5,11 +5,14 @@ from flask import url_for
 from src import db
 
 
-class Post(db.Document):
+class Vote(db.Document):
+    CUSTOM_VOTE_ID = 6
+    voteFields = {1: 'Easy to read', 2: 'Easy to use', 3: 'Internet of things opportunities',
+                  4: 'Asynchronous coding benefits', 5: 'Multiparadigm approach bests Java'}
+
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-    title = db.StringField(max_length=255, required=True)
-    slug = db.StringField(max_length=255, required=True)
-    body = db.StringField(required=True)
+    ip = db.StringField(max_length=15, required=True)
+    votes = db.DictField(required=True)
 
     def get_absolute_url(self):
         return url_for('post', kwargs={"slug": self.slug})
@@ -17,9 +20,16 @@ class Post(db.Document):
     def __unicode__(self):
         return self.title
 
+    def get_vote_title(self, vote_id):
+        """
+        Get title by identifier
+        :type vote_id: int
+        """
+        vote_id = int(vote_id)
+        return self.voteFields[vote_id] if vote_id in self.voteFields.keys() else None
+
     meta = {
         'allow_inheritance': True,
-        'indexes': ['-created_at', 'slug'],
+        'indexes': ['-created_at', 'ip'],
         'ordering': ['-created_at']
     }
-
--- src/views.py
+++ src/views.py
@@ -2,6 +2,7 @@ __author__ = 'miholeus'
 
 from flask import Blueprint, request, render_template, jsonify
 from flask.views import MethodView
+from models import Vote
 
 bp = Blueprint('votes', __name__, template_folder='templates')
 
@@ -21,9 +22,22 @@ class AboutView(MethodView):
 class VoteView(MethodView):
     @staticmethod
     def post():
-        # print request
-        print request.get_data()
-        return jsonify({'status': 'ok'})
+        values = {}
+        vote_value = request.form['vote']
+        vote = Vote()
+        for item in request.form.getlist('value[]'):
+            if vote.get_vote_title(item) is not None:
+                values[item] = vote.get_vote_title(item)
+            elif int(item) == Vote.CUSTOM_VOTE_ID and vote_value:
+                values[item] = vote_value
+        if values:
+            # add vote
+            vote.ip = request.remote_addr
+            vote.votes = values
+            vote.save()
+            return jsonify({'status': 'ok'})
+        else:
+            return jsonify({'status': 'error', 'message': 'Choose any option to vote'})
 
 
 # Register the urls
